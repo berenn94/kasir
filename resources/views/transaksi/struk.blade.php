11402543e0f9adb5174e6fd7dc7ecc2608cb4b60
baron <baronnugroho944@gmail.com>
2024-02-25 13:38:36 +0700

    asw

---
+++ resources/views/transaksi/struk.blade.php
@@ -0,0 +1,37 @@
+<body>
+    <p>Toko Kasirku</p>
+    <p>Jl. Raya Kebanjiran Kab Suka suka</p>
+    <p> Kasir : 
+            @if(isset($kasir->name))
+                {{$kasir->name}}
+            @else
+                Data Tidak Tersedia
+            @endif
+    </p>
+    <p>Tanggal :{{$ambil->tanggal_beli}}</p>
+    <p>===================================</p>
+    <table style="margin-top:-10px">
+    @foreach($transaksi as $u)
+        <tr>
+            <td style="padding-right:50px">{{$u->nama_barang}}</td>
+            <td style="padding-right:50px">{{$u->jumlah_beli}}</td>
+            <td style="padding-right:50px">{{$u->harga_barang}}</td>
+            <td style="padding-right:50px">{{$u->total_harga}}</td>
+        </tr>
+    @endforeach
+        <tr>
+            <td colspan="3" style="text-align:right;padding-right:50px">Total Harga</td>
+            <td>{{$jumlah}}</td>
+        </tr>
+        <tr>
+            <td colspan="3" style="text-align:right;padding-right:50px">Bayar</td>
+            <td>{{$kembalian->bayar}}</td>
+        </tr>
+        <tr>
+            <td colspan="3" style="text-align:right;padding-right:50px">Kembalian</td>
+            <td>{{$kembalian->kembalian}}</td>
+        </tr>
+    </table>
+    <p>===================================</p>
+    <p>Terimakasih Telah berbelanja di tempat kami <br>semoga menyenangkan</p>
+</body>
